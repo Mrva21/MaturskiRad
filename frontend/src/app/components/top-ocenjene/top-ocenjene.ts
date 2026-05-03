efa1dbfba9dfd241a3b5506abd45c9759e2fba5b
Mrva21 <nemanja.lazovic4@gmail.com>
2026-05-03 15:13:05 +0200

    changes

--- frontend/src/app/components/top-ocenjene/top-ocenjene.ts
+++ frontend/src/app/components/top-ocenjene/top-ocenjene.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Rawg } from '../../services/rawg';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-top-ocenjene',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './top-ocenjene.html',
   styleUrl: './top-ocenjene.css',
 })
 export class TopOcenjene {
+  constructor(private servis: Rawg, private cd: ChangeDetectorRef) {}
 
+  selectedPeriod = 30;
+  games: any[] = [];
+
+  ngOnInit() {
+    this.loadGames();
+  }
+
+  loadGames() {
+    this.servis.getTopRatedGames(this.selectedPeriod)
+      .subscribe((data: any) => {
+        this.games = data.results;
+        this.cd.detectChanges();
+      });
+  }
+
+  changePeriod(days: number) {
+    this.selectedPeriod = days;
+    this.loadGames();
+  }
 }
